819c6e9ee4e1fcfcd065b29fa715e8b1b2d82285
AncientTangeloes <119451157+AncientTangeloes@users.noreply.github.com>
2024-04-30 18:51:44 -0500

    Add project files.

---
+++ ClassSchedule/Migrations/20240326153947_herecomedatboy.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ch12Ex1ClassSchedule.Migrations
+{
+    /// <inheritdoc />
+    public partial class herecomedatboy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Classes",
+                keyColumn: "ClassId",
+                keyValue: 1,
+                column: "MilitaryTime",
+                value: "1500");
+
+            migrationBuilder.UpdateData(
+                table: "Classes",
+                keyColumn: "ClassId",
+                keyValue: 6,
+                column: "DayId",
+                value: 5);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Classes",
+                keyColumn: "ClassId",
+                keyValue: 1,
+                column: "MilitaryTime",
+                value: "1100");
+
+            migrationBuilder.UpdateData(
+                table: "Classes",
+                keyColumn: "ClassId",
+                keyValue: 6,
+                column: "DayId",
+                value: 3);
+        }
+    }
+}
---
+++ ClassSchedule/Models/DataLayer/ClassScheduleUnitOfWork.cs
@@ -0,0 +1,55 @@
+﻿namespace ClassSchedule.Models
+{
+    public class ClassScheduleUnitOfWork : IClassScheduleUnitOfWork
+    {
+        private ClassScheduleContext context { get; set; }
+
+        private Repository<Class> classData { get; set; }
+        public Repository<Class> Classes
+        { 
+            get
+            {
+                if (classData == null)
+                {
+                    classData = new Repository<Class>(context);
+                }
+                return classData;
+            }
+        }
+        private Repository<Teacher> teacherData { get; set; }
+        public Repository<Teacher> Teachers
+        {
+            get
+            {
+                if (teacherData == null)
+                {
+                    teacherData = new Repository<Teacher>(context);
+                }
+                return teacherData;
+            }
+        }
+        private Repository<Day> dayData { get; set; }
+        public Repository<Day> Days
+        {
+            get
+            {
+                if (dayData == null)
+                {
+                    dayData = new Repository<Day>(context);
+                }
+                return dayData;
+            }
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+
+        public ClassScheduleUnitOfWork(ClassScheduleContext ctx)
+        {
+            this.context = ctx;
+        }
+    }
+}
